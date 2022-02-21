b83e4b7118ad101c67c291f58f4949b81f92ab23
tollef jørgensen <tollefj@gmail.com>
2022-02-21 13:21:04 +0100

    login prompt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import { useState, useEffect } from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { Button, Fab } from '@mui/material'
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 
-function App() {
+const FloatWithIcon = ({Icon, text, ...props }) => (
+  <Fab variant="extended" {...props}>
+    <Icon sx={{ mr: 1 }}/>
+    {text}
+  </Fab>
+)
+
+function App({loginConfig}) {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [loginPrompt, setLoginPrompt] = useState(false);
+
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+    });
+    // Make sure we un-register Firebase observer
+    // when the component unmounts.
+    return () => unregisterAuthObserver(); 
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <FloatWithIcon
+          Icon={LoginIcon}
+          text="Login"
+          onClick={() => setLoginPrompt(true)}
+        />
+        {loginPrompt && (
+          <StyledFirebaseAuth uiConfig={loginConfig} firebaseAuth={firebase.auth()} />
+        )}
+      </div>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>DART Internal Seminars</h1>
+      <p>Welcome {firebase.auth().currentUser.displayName}</p>
+      <FloatWithIcon
+        Icon={LogoutIcon}
+        text="Sign out"
+        onClick={() => firebase.auth().signOut()}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,51 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  EmailAuthProvider,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_MSG,
+  appId: process.env.REACT_APP_ID
+};
+firebase.initializeApp(firebaseConfig);
+
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  signInOptions: [
+    EmailAuthProvider.PROVIDER_ID,
+    {
+      provider: GoogleAuthProvider.PROVIDER_ID,
+      scopes: [
+        'https://www.googleapis.com/auth/contacts.readonly'
+      ],
+      customParameters: {
+        prompt: 'select_account'
+      }
+    },
+    GithubAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  }
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App loginConfig={uiConfig}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
